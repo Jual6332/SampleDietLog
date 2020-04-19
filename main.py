58b2f8f0d72595d636c105619c79d87308f2f7b1
Justin Alvey <jual6332@colorado.edu>
2020-04-19 22:39:21 +0000

    Minor changes to the DailyLog class and output display

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ class DayLog:
     self.date = dat;
     self.burned = burn;
     self.consumed = consum;
+    self.net = burn-consum;
 
   def get_calories_burned(self):
     return self.burned;
 
   def get_calories_consumed(self):
     return self.consumed;
+
+  def get_calories_net(self):
+    return self.net;
 
   def get_date(self):
     return self.date;
@@ -30,19 +34,19 @@ july_2020, august_2020, september_2020 = [], [], []
 october_2020, november_2020, december_2020 = [], [], []
 
 # Define April Data
-april19 = DayLog("4/19",170,960*2+90)
+april19 = DayLog("4/19",170+105*18,960*2+90)
 april20 = DayLog("4/20",0,0)
 
 april_2020.append(april19)
 april_2020.append(april20)
 
 # Display Data --- April
-print("Calories Burned in April:")
-print("=========================")
+print("Month of April:")
+print("===============")
 for log in april_2020:
-  print(log.get_date() + ": " + str(log.get_calories_burned())+ " calories.")
+  print(log.get_date() + ": " + str(log.get_calories_burned())+ " calories burned.")
+  print("      "+str(log.get_calories_consumed())+" calories consumed.")
+  print("Net:  "+str(log.get_calories_net())+" calories.")
 print("\n")
 
 # Display Data --- May
-print("Calories Burned in April:")
-print("=========================")

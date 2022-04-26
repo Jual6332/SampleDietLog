b9c0ca8cc3d7a2ab7943bd1b3dbc8332636e1eba
jual6332@colorado.edu <jual6332@colorado.edu>
2022-04-25 20:52:27 -0400

    April data storage updated successfully

--- MonthlyData2020/April_data.py
+++ MonthlyData2020/April_data.py
@@ -4,6 +4,21 @@ from Daylog_class import DayLog
 april_2020 = []
 
 # Define April Data
-april_2020.append(DayLog("4/19",170+1982,960*2+90))
-april_2020.append(DayLog("4/21",295+1982+159,2220))
-april_2020.append(DayLog("4/27",295+1982,1655))
+current_rmr = 1982 # Resting metabolic rate
+
+# Individual Logs 1-3
+log1, log2, log3 = DayLog(), DayLog(), DayLog()
+log1.set_date("4/19")
+log1.set_calories_burned(current_rmr)
+log1.set_calories_consumed(960*2+90)
+log1.add_exercise(170)
+
+log2.set_date("4/21")
+log2.set_calories_burned(current_rmr)
+log2.set_calories_consumed(2220)
+log2.add_exercise(295+159)
+
+# Store Data 
+april_2020.append(log1)
+april_2020.append(log2)
+#april_2020.append(DayLog("4/27",295+1982,1655))

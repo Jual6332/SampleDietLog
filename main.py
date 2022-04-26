4d2ea3b8099bf268c63550b477ed2e1e80814068
jual6332@colorado.edu <jual6332@colorado.edu>
2022-04-25 20:18:42 -0400

    Improved comments on main.py script

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+# Main Script
+# Goal: Calculate Monthly net caloric difference per year 
+# Drawback: Limited personal data
 from helper_functions import *
 
-from MonthlyData2020.April_data import april_2020
-from MonthlyData2020.May_data import may_2020
+#from MonthlyData2020.April_data import april_2020
+#from MonthlyData2020.May_data import may_2020
 
-# Initialisation
+# Initialization of empty monthly data
 august_2020, september_2020 = [], []
 october_2020, november_2020, december_2020 = [], [], []
 
-# Calculate Average Net per Month
-may_avg_net = calculateMonthlyAvgNet(may_2020)
+# Calculate average net caloric difference per month
 april_avg_net = calculateMonthlyAvgNet(april_2020)
+may_avg_net = calculateMonthlyAvgNet(may_2020)
 
-# Display Data per Month
+# Display Data for the user
+# 1. Display data by month
+# 2. Display more recent data first
 printMonthlyData("May",may_2020,may_avg_net)
 print("\n")
 printMonthlyData("April",april_2020,april_avg_net)

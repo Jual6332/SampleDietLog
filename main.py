0ed3da97c3d16bc6ce8ff01018c2f927e25556b6
jual6332@colorado.edu <jual6332@colorado.edu>
2022-04-25 20:53:21 -0400

    Commented out Maydata request because data had not been updated yet

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Drawback: Limited personal data
 from helper_functions import *
 
-#from MonthlyData2020.April_data import april_2020
+from MonthlyData2020.April_data import april_2020
 #from MonthlyData2020.May_data import may_2020
 
 # Initialization of empty monthly data
@@ -12,11 +12,11 @@ october_2020, november_2020, december_2020 = [], [], []
 
 # Calculate average net caloric difference per month
 april_avg_net = calculateMonthlyAvgNet(april_2020)
-may_avg_net = calculateMonthlyAvgNet(may_2020)
+#may_avg_net = calculateMonthlyAvgNet(may_2020)
 
 # Display Data for the user
 # 1. Display data by month
 # 2. Display more recent data first
-printMonthlyData("May",may_2020,may_avg_net)
+#printMonthlyData("May",may_2020,may_avg_net)
 print("\n")
 printMonthlyData("April",april_2020,april_avg_net)
